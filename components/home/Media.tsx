864ed458a297424cd14938cc41d2ac409e6598fd
Nick Alekhine <nick.alekhine@gmail.com>
2025-02-11 09:43:41 -0500

    add chain react presentation

--- components/home/Media.tsx
+++ components/home/Media.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native";
+import { View } from "react-native";
 import { ThemedText } from "../ThemedText";
 import { Spacing } from "../design-system/spacing";
 import { ExternalLink } from "../ExternalLink";
@@ -15,12 +15,29 @@ export function Media() {
         Conferences & Podcasts
       </ThemedText>
 
-      <ExternalLink href="https://www.youtube.com/watch?v=ik1eMi0Raro">
-        <ThemedText type="semibold">Chain React 2023{"\n"}</ThemedText>
-        <ThemedText>
-          Building NTWRK’s livestream shopping application
-        </ThemedText>
-      </ExternalLink>
+      <View>
+        <ExternalLink href="https://www.youtube.com/watch?v=ik1eMi0Raro">
+          <ThemedText type="semibold">Chain React 2023{"\n"}</ThemedText>
+          <ThemedText>
+            Building NTWRK’s livestream shopping application
+          </ThemedText>
+        </ExternalLink>
+        <ExternalLink
+          href="/chain-react-2023.pdf"
+          style={{ marginTop: Spacing.S2 }}
+        >
+          <ThemedText type="caption">
+            Slides for the presentation can be found{" "}
+            <ThemedText
+              type="caption"
+              style={{ textDecorationLine: "underline" }}
+            >
+              here
+            </ThemedText>
+            .
+          </ThemedText>
+        </ExternalLink>
+      </View>
 
       <ExternalLink href="https://reactnativeradio.com/episodes/rnr-272-real-life-react-native-nick-alekhine-at-ntwrk-h0wCv_0I">
         <ThemedText type="semibold">React Native Radio{"\n"}</ThemedText>
